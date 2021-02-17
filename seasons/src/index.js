ec775c12732503067cf8cfa45da62d9f4f0578c9
Mario Alvarez <m7alvarez4@gmail.com>
2021-02-17 11:05:08 -0800

    Initilize state through constructor

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    // one way of initializing state
+    constructor(props) {
+        super(props);
+        
+        // Rule of State: state must be initialize when compentn is created
+        this.state = { lat: null }; 
+    }
+
+    // React says we have to define a render method for each app component
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
