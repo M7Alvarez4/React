b43a81af7f22805172a29d434c64777064afc4cb
Mario Alvarez <m7alvarez4@gmail.com>
2021-02-19 13:08:09 -0800

    use promises to verify extraction of data from api

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-    onSearchSubmit(term) {
-        console.log(term);
+    async onSearchSubmit(term) {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID BG9hNmbFlPf31NvI6LdfHP7vGBgfNHnkRzoD_x4Wsp8'
+            }
+        })
+        .then((response) => {
+            console.log(response.data.results);
+        });
     }
     
     render () {
